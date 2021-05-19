b0dd06758c0f11ec461cc4dba7b42a59998417ab
__siddhant__ <75603064+sabrina214@users.noreply.github.com>
2021-05-19 17:02:21 +0530

    Add files via upload

---
+++ railfence.py
@@ -0,0 +1,55 @@
+def encrypt(plain_text):
+    n = len(plain_text)
+    # cipher_text = [0] * n
+    cipher_text = ''
+
+    '''
+    0 2 4 6 8 10
+    1 3 5 7 9
+    '''
+    
+    k = (n // 2) + (n % 2)
+    for i in range(n):
+        if i < k:
+            # cipher_text[i] = (plain_text[i*2])
+            cipher_text += (plain_text[i*2])
+        else:
+            # cipher_text[i] = (plain_text[i-(k-1)])
+            cipher_text += (plain_text[i-(k-1)])
+            k -= 1
+    return cipher_text
+
+
+def decrypt(cipher_text):
+    n = len(cipher_text)
+    decrypted_text = ''
+
+    '''
+    0 6 1 7 2 8 3 9 4 10 5
+    | | | | | | | | |  | |
+    0 1 2 3 4 5 6 7 8  9 10
+    '''
+
+    k = (n // 2) + (n % 2)
+    for i in range(n):
+        if i % 2 == 0:
+            # decrypted_text[i] = (cipher_text[i//2])
+            decrypted_text += (cipher_text[i//2])
+        else:
+            # decrypted_text[i] = (cipher_text[i+(k-1)])
+            decrypted_text += (cipher_text[i+(k-1)])
+            k -= 1
+    return decrypted_text
+
+
+if __name__ == '__main__':
+    # plain_text = 'meet me at the park'.replace(' ', '')
+    plain_text = input('Plaintext: ')
+
+    print('Encrypting...')
+    cipher_text = encrypt(plain_text)
+    print('Ciphertext:',cipher_text)
+
+    print('\nDecrypting...')
+    decrypted_text = decrypt(cipher_text)
+    print('Decrypted-text:',decrypted_text)
