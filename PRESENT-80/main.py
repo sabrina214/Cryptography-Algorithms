897adc7178bc84e87442e845cc0d35d6b9e5d437
__siddhant__ <75603064+sabrina214@users.noreply.github.com>
2022-02-10 14:45:14 +0530

    Driver file made more friendly

--- PRESENT-80/main.py
+++ PRESENT-80/main.py
@@ -1,8 +1,41 @@
 from present import PRESENT
+from getpass import getpass
+import sys
 
 
-# 80-bit key
-key = "0123456789"
+if __name__ == '__main__':
+  plaintext = input("Plaintext(press enter for default text): ") or "Spiderman No way home"
+  
+  attempts = 3
+  # 80-bit key
+  # key = "0123456789"
+  while attempts:
+    key = getpass("Key(You won't be able to see as you type): ")
+    if len(key) != 10:
+      attempts -= 1
+      print('Key length must be 10. Attempts left', attempts, '\n')
+    else: break
+  if not attempts: sys.exit("Try again with valid key")
+  
+  cipher = PRESENT(plaintext, key)
 
-cipher = PRESENT(key)
-print(cipher.round_keys)
+  verbose = input('Do you want to see verbose output?[Y/n] ').lower().startswith("y")
+
+  print('\nENCRYPTED TEXT:', cipher.encrypt(verbose))
+
+  attempts = 3
+  while attempts:
+    key = getpass("Key(Enter key to decrypt): ")
+    if len(key) != 10:
+      attempts -= 1
+      print('Key length must be 10. Attempts left', attempts, '\n')
+    elif key != cipher.key:
+      attempts -= 1
+      print('Key invalid! Attempts left', attempts, '\n')
+    else: break
+
+  if not attempts: sys.exit("Try again with valid key")
+  
+  verbose = input('Do you want to see verbose output?[Y/n] ').lower().startswith("y")
+
+  print('\nDECRYPTED TEXT:', cipher.decrypt(verbose), '\n')
